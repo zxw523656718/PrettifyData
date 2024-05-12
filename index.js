49b4f3f89b7932a93d14444573e3f45d6779c64d
zxw523656718 <36107025+zxw523656718@users.noreply.github.com>
2024-05-12 18:38:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-
 export { default as formatCurrency } from './currencyFormatter';
 export { default as formatDate } from './dateFormatter';
+export { default as formatPercent } from './percentFormatter';
+export { default as formatTime } from './timeFormatter';
+export { default as numberAbbreviator } from './numberAbbreviator';
+export { default as ChainFormatter } from './chainFormatter';
